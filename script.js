1d91c95773f4a98feedcf1e0e632c2775625013d
Rajesh Tikadar <tikadarrajesh2115@gmail.com>
2026-06-05 11:41:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+
+    for(tablink of tablinks){
+        tablink.classList.remove("active-links");
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-links");
+
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+function showProject(){
+
+  document.querySelector(".hidden-work").classList.add("show");
+
+  document.querySelector(".btn").style.display = "none";
+}
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxjwGKcEKPac71bPg2Xjnc3YF5dhj0xITSA_d3CzeumMUwa0IXw2m6tBVuXCjM86dMQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    fetch(scriptURL, {
+        method: 'POST',
+        body: new FormData(form)
+    })
+    .then(response => {
+        msg.innerHTML="Message sent Successfully"
+        setTimeout(function(){
+            msg.innerHTML=""
+        },3000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+var sidemenu=document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
